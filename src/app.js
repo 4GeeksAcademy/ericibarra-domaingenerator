7879b5af30eaafd9389a34a9779b15fe01569bc3
Asenius <100125344+Asenius@users.noreply.github.com>
2025-01-13 20:30:14 +0000

    todo hecho

--- src/app.js
+++ src/app.js
@@ -1,11 +1,31 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+let domain = ['.com', '.es'];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let result = [];
+
+for (let i = 0; i < pronoun.length; i++){
+
+  for (let j = 0; j < adj.length; j++){
+
+    for (let k = 0; k < noun.length; k++){
+
+      for (let l = 0; l < domain.length; l++){
+
+        result.push(pronoun[i] + adj[j] + noun[k] + domain[l]);
+
+      }
+
+    }
+
+  }
+
+}
+
+for(let i = 0; i < result.length; i++){
+
+  console.log(result[i]);
+
+}
